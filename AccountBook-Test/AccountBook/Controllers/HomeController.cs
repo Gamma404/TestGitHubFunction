e3008c135507b2071f0e13b8d8911cffedc8e84e
Gamma404 <shihjheng0522@gmail.com>
2017-08-10 22:56:48 +0800

    ASP.NET MVC 第一天作業階段性版本
    TODO:
    1.因為第一天使用,一開始沒命名好,GitHub上面的目錄不好
    但是怕來不及交作業先傳一版,等等第二要換成正確目錄
    2.歷史紀錄的標題沒顯示：需學習HTML的這個怎麼組
    3.Demo有說這是模擬真實環境,要用設計給的HTML當基底,不該用MVC的範本,這不是很懂,但是應該就是要把_Layout.cshtml換掉吧
    4.F5跟Shift F5的執行結果好像不一樣,時好時壞這個要再請教

--- AccountBook-Test/AccountBook/Controllers/HomeController.cs
+++ AccountBook-Test/AccountBook/Controllers/HomeController.cs
@@ -3,14 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AccountBook.Models.ViewModels;
 
 namespace AccountBook.Controllers
 {
     public class HomeController : Controller
     {
+        public enum AccountType
+        { Income, Expenture };
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ChildActHistoryAction()
+        {
+            int index = 0;
+            var accountLists = new List<RecordOfAccountModel>()
+            {
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Expenture,
+                    Money = 50m,
+                    Date = DateTime.Parse("2017-08-05"),
+                    Description = "早餐-漢堡蛋＋紅茶"
+                },
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Expenture,
+                    Money = 100m,
+                    Date = DateTime.Parse("2017-08-05"),
+                    Description = "午餐-雞腿便當"
+                },
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Expenture,
+                    Money = 45m,
+                    Date = DateTime.Parse("2017-08-05"),
+                    Description = "文具-魔擦筆x1"
+                },
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Income,
+                    Money = 1050m,
+                    Date = DateTime.Parse("2017-08-05"),
+                    Description = "7月管理委員會退費"
+                },
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Expenture,
+                    Money = 30m,
+                    Date = DateTime.Parse("2017-08-06"),
+                    Description = "早餐-漢堡蛋"
+                },
+                new RecordOfAccountModel()
+                {
+                    Id = ++index,
+                    Type = (int)AccountType.Expenture,
+                    Money = 90m,
+                    Date = DateTime.Parse("2017-08-06"),
+                    Description = "午餐-排骨便當"
+                },
+            };
+
+            return View(accountLists);
         }
 
         public ActionResult About()
---
+++ AccountBook-Test/AccountBook/Models/ViewModels/RecordOfAccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountBook.Models.ViewModels
+{
+    public class RecordOfAccountModel
+    {
+        public int Id { get; set; }
+        public int Type { get; set; }
+        public decimal Money { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
